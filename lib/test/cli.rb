e9e18e13a30bc906f28387b298e2292900488e98
Aglae Verduzco <aglae.cv@gmail.com>
2019-10-02 01:46:46 +0000

    modified cli file

--- lib/test/cli.rb
+++ lib/test/cli.rb
@@ -6,3 +6,14 @@ module Test
     # Your code goes here...
   end
 end
+
+
+#Welcome to Houston Fun.
+#the following entertainment will be for adults only.
+#please select one of the following 
+#if you are needing assistnace in Team building - need to unite the work group?
+#if you are wanting to plan a fun adventure with friends
+#if you need ideas for date night
+#to list all availabilities
+--#to list activities that permit alcohol
+--#to list activites
