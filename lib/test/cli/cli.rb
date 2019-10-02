4ade1b9b7ac2ebdd70290624cf67561bcec93899
Aglae Verduzco <aglae.cv@gmail.com>
2019-10-02 01:58:43 +0000

    modified activities file

--- lib/test/cli/cli.rb
+++ lib/test/cli/cli.rb
@@ -11,8 +11,8 @@ end
 #Welcome to Houston Fun.
 #the following entertainment will be for adults only.
 #please select one of the following 
-#if you are needing assistnace in Team building - need to unite the work group?
-#if you are wanting to plan a fun adventure with friends
+#if you are needing assistance in Team building activities - need to unite the work group?
+#if you are wanting to plan a fun adventure with friends - adventure seekers
 #if you need ideas for date night
 #to list all availabilities
 --#to list activities that permit alcohol
